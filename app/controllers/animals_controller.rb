63e1bf1c49efe0e446a47140dabdb4a8468beb1f
learn <learn@learn10.local>
2019-11-14 14:32:23 -0800

    basic crud for animals

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,6 @@
 class AnimalsController < ApplicationController
+    before_action :set_animal, only: [:show, :update, :destroy]
+
     def index
         animals = Animal.all
         render json: animals
@@ -11,9 +13,27 @@ class AnimalsController < ApplicationController
             render json: new_animal.errors, status: 422
         end
     end
-    
+    def destroy
+        if @animal.destroy
+            render json: @animal, status: 204
+        else
+            render json: @animal, status: 404
+        end
+    end
+
+    def update
+        if @animal.update(animal_params)
+            render json: @animal, status: 200
+        else
+            render json: @animal, status: 404
+        end
+    end
+
     private
     def animal_params
         params.require(:animal).permit(:common_name, :latin_name, :kingdom)
+    end
+    def set_animal
+        @animal = Animal.find(params[:id])
     end
 end
--- spec/requests/animal_cruds_spec.rb
+++ spec/requests/animal_cruds_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "AnimalCruds", type: :request do
-    let!(:animal){Animal.create(common_name: "Lion", latin_name: "Panthera leo", kingdom: "	Animalia")}
+RSpec.describe "Animal API", type: :request do
+    let!(:animals){Animal.create(common_name: "Lion", latin_name: "Panthera leo", kingdom: "	Animalia")}
+    let(:animal_id){animals.id}
 
-  describe "GET /animal" do
+  describe "GET /animals" do
+    before { get animals_path}
     it "should return a 200 http response" do
-      get animals_path
       expect(response).to have_http_status(200)
     end
     it "should return Lion" do
-      get animals_path
       animals = JSON.parse(response.body)
       expect(animals.first['common_name']).to eq("Lion")
     end
   end
 
-  describe "POST /animal" do
+  describe "POST /animals" do
       it "should creates an animal " do
           headers = {
             "ACCEPT" => "application/json"
@@ -23,6 +23,28 @@ RSpec.describe "AnimalCruds", type: :request do
         body = {animal: {common_name: "Cat", latin_name: "Felis catus", kingdom: "	Animalia"}}
         post animals_path, params: body, headers: headers
         expect(response).to have_http_status(201)
+      end
+  end
+
+  describe "DELETE /animals/:id" do
+      before { delete "/animals/#{animal_id}"}
+      it "should return status code 204" do
+          expect(response).to have_http_status(204)
+      end
+      # it "should return status code 404 " do
+      #     delete "/animals/0"
+      #     expect(response).to have_http_status(204)
+      # end
+  end
+
+  describe "PUT /animals/:id" do
+      it "should return status code 204" do
+        headers = {
+            "ACCEPT" => "application/json"
+        }
+        body = {animal: {common_name: "Cat", latin_name: "Felis catus", kingdom: "	Animalia"}}
+        put "/animals/#{animal_id}", params: body, headers: headers
+        expect(response).to have_http_status(200)
       end
   end
 end
